57e0a5d1a3c8b6913d6374b5a3b015f6848a7ce1
Malka <Malka.in.red@gmail.com>
2019-10-28 14:14:27 -0400

    remove comments

--- client/index.js
+++ client/index.js
@@ -8,5 +8,5 @@ ReactDOM.render(
   <Provider store={store}>  
     <div>Hello, world!</div>
   </Provider>,
-  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+  document.getElementById('app')
 );
