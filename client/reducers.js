051a62e107b2cf463e4794db30ebe4a3fedd5f91
Malka <Malka.in.red@gmail.com>
2020-02-21 11:58:39 -0500

    add portfolio and transaction fetch to store

--- client/reducers.js
+++ client/reducers.js
@@ -2,24 +2,41 @@ import axios from 'axios'
 
 const LOGIN_USER = 'LOGIN'
 const BUY_STOCK = 'BUY_STOCK'
+const GET_TRANSACTIONS = 'GET_TRANSACTIONS'
+const GET_PORTFOLIO = 'GET_PORTFOLIO'
 
 const loginUser = userInfo => ({type: LOGIN_USER, userInfo})
 const buyStock = stockInfo => ({type: BUY_STOCK, stockInfo})
+const getTransactions = (transactions) => ({type: GET_TRANSACTIONS, transactions})
+const getPortfolio = (portfolio) => ({type: GET_PORTFOLIO, portfolio})
 
 const defaultState = {
     loggedIn: false,
     cashBalance: 0,
+    transactions: [],
+    portfolio: [],
 }
 
 export const login = () => async dispatch => {
   try {
+
+    // login 
     const res = await axios.get('/auth/me')
     dispatch(loginUser(res.data))
+
+    // grab user data and set to store
+    let portfolio = await axios.get('/api/portfolio')
+    dispatch(getPortfolio(portfolio.data))
+    
+    let transactions = await axios.get('/api/transactions')
+    dispatch(getTransactions(transactions.data))
+
   } catch (err) {
     console.error(err)
   }
 }
 
+// buy stock thunk
 export const buy = (stockInfo) => async dispatch => {
   try {
     await axios.patch('/api/buy', stockInfo)
@@ -34,7 +51,16 @@ export default function(state = defaultState, action) {
     case LOGIN_USER:
       return {loggedIn: true, cashBalance: action.userInfo.cashBalance}
     case BUY_STOCK:
-        return {...state, cashBalance: state.cashBalance - action.stockInfo.totalPrice}
+      return {
+        ...state, 
+        cashBalance: state.cashBalance - action.stockInfo.totalPrice,
+        transactions: [...state.transactions, action.stockInfo],
+        portfolio: [...state.portfolio, {code: action.stockInfo.code, quantity: action.stockInfo.quantity}]
+      }
+    case GET_TRANSACTIONS: 
+      return {...state, transactions: action.transactions}
+    case GET_PORTFOLIO:
+      return {...state, portfolio: action.portfolio}
     default:
       return state
   }
