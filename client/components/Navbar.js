381464ba8a516eabc3386e5f34ee3028506d7dbf
Malka <Malka.in.red@gmail.com>
2020-02-21 16:17:36 -0500

    style navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,19 +2,31 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
+const styles = {
+  nav: {
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  link: {
+    color: "black",
+    padding: "20px",
+    textDecoration: "none"
+  }
+}
+
 const Navbar = props => (
-      <nav>
+      <nav style={styles.nav}>
         {!props.isLoggedIn ? (
           <div>
             {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
+            <Link style={styles.link} to="/login">Login</Link>
+            <Link style={styles.link} to="/signup">Signup</Link>
           </div>
         ) : (
           <div>
             {/* The navbar will show these links after you log in */}
-              <Link to="/transactions">transactions</Link>
-              <Link to="/portfolio">Portflio</Link>
+              <Link style={styles.link} to="/transactions">transactions</Link>
+              <Link style={styles.link} to="/portfolio">Portflio</Link>
           </div>
         )}
       </nav>
