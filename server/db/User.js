70ae5aa02436bbe643d6dbd6762a4fb4edc838bf
Malka <Malka.in.red@gmail.com>
2020-02-23 10:57:39 -0500

    Revert "change backend to double quotes"
    This reverts commit 6be28f8896ee417a9f821e1f293647494c4b4f69.

--- server/db/User.js
+++ server/db/User.js
@@ -1,10 +1,10 @@
-const crypto = require("crypto")
-const _ = require("lodash")
-const Sequelize = require("sequelize")
+const crypto = require('crypto')
+const _ = require('lodash')
+const Sequelize = require('sequelize')
 
-const db = require("./db")
+const db = require('./db')
 
-const User = db.define("user", {
+const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
     unique: true,
@@ -42,23 +42,23 @@ User.prototype.correctPassword = function (candidatePassword) {
 }
 
 User.prototype.sanitize = function () {
-  return _.omit(this.toJSON(), ["password", "salt"])
+  return _.omit(this.toJSON(), ['password', 'salt'])
 }
 
 // class methods
 User.generateSalt = function () {
-  return crypto.randomBytes(16).toString("base64")
+  return crypto.randomBytes(16).toString('base64')
 }
 
 User.encryptPassword = function (plainText, salt) {
-  const hash = crypto.createHash("sha1")
+  const hash = crypto.createHash('sha1')
   hash.update(plainText)
   hash.update(salt)
-  return hash.digest("hex")
+  return hash.digest('hex')
 }
 
 function setSaltAndPassword (user) {
-  if (user.changed("password")) {
+  if (user.changed('password')) {
     user.salt = User.generateSalt()
     user.password = User.encryptPassword(user.password, user.salt)
   }
--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
-const User = require("./User")
-const Transaction = require("./Transaction")
-const db = require("./db")
+const User = require('./User')
+const Transaction = require('./Transaction')
+const db = require('./db')
 
 User.hasMany(Transaction)
 module.exports = {db, User, Transaction}
